e972ce0cf34df5842bf289cc1884b79b72475d5e
sideeqhammed <sideeqhammed05.com>
2025-07-18 15:04:29 +0100

    Finished control-flow project

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -3,6 +3,25 @@ priority = (input("Priority (high/medium/low): ")).lower()
 time_bound = (input("Is it time-bound? (yes/no): ")).lower()
 
 match time_bound:
-  case "yes": print(task, "is a", priority, "priority task that requires immediate attention today!")
-  case "no": print(task, "is a", priority, "priority task. Consider completing it when you have free time.")
+  case "high": 
+    if time_bound == "yes":
+      print(task, "is a high priority task that requires immediate attention today!")
+    elif time_bound == "no":
+      print(task, "is a high priority task that requires immediate attention today!")
+    else : print("Task's time-bound property not identified")
+
+  case "medium": 
+    if time_bound == "yes":
+      print(task, "is a medium priority task that requires immediate attention today!")
+    elif time_bound == "no":
+      print(task, "is a medium priority task that requires immediate attention today!")
+    else : print("Task's time-bound property not identified")
+  
+  case "low": 
+    if time_bound == "yes":
+      print(task, "is a low priority task that requires immediate attention today!")
+    elif time_bound == "no":
+      print(task, "is a low priority task that requires immediate attention today!")
+    else : print("Task's time-bound property not identified")
+
   case _: print("Task priority not identified")
