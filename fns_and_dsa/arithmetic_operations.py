bec4e5462059ee50b7fbe41e8d7aa5dd269def7e
sideeqhammed <sideeqhammed05.com>
2025-07-27 07:03:00 +0100

    added fns_and_dsa

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,9 +1,9 @@
 def perform_operation(num1, num2, operation):
-  if operation == add:
+  if operation == "add":
     return(num1 + num2)
-  elif operation == "subtract"
+  elif operation == "subtract":
     return(num1 - num2)
-  elif operation == "mulitply"
+  elif operation == "mulitply":
     return(num1 * num2)
   elif operation == "divide": 
     if num2 != 0:
