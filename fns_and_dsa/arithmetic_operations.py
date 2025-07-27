f11e91cc9d1fa9ad8a4bbf714a54784f7faa3750
sideeqhammed <sideeqhammed05.com>
2025-07-27 07:24:46 +0100

    added fns_and_dsa

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,13 +1,10 @@
 def perform_operation(num1, num2, operation):
-  if operation == "add":
-    return(num1 + num2)
-  elif operation == "subtract":
-    return(num1 - num2)
-  elif operation == "multiply":
-    return(num1 * num2)
-  elif operation == "divide": 
-    if num2 == 0:
-      return("Zero division error")
-    else: return(num1 / num2)
-  else: return("Invalid operator provided")
+  match operation:
+    case "add": return(num1 + num2)
+    case "subtract": return(num1 - num2)
+    case "mulitply": return(num1 * num2)
+    case "divide": 
+      if num2 != 0:
+        return(num1 / num2)
+      else:return("Zero division error")
     
